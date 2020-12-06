d4342cd32b1daaeed010917d9dec04cb4186ddd6
GCarrell <GCarrell@users.noreply.github.com>
2020-12-06 17:15:54 +0000

    Testing features now working Minor GUI colour changes

--- MusicManagerTESTS/CRUDTESTS.cs
+++ MusicManagerTESTS/CRUDTESTS.cs
@@ -17,7 +17,11 @@ namespace MusicManagerTESTS
                     from c in db.Users
                     where c.UserName == "Padge"
                     select c;
-
+                var selectedTabs =
+                    from t in db.Tabs
+                    where t.TabName == "Through The Looking Glass"
+                    select t;
+                db.Tabs.RemoveRange(selectedTabs);
                 db.Users.RemoveRange(selectedUsers);
                 db.SaveChanges();
             }
@@ -32,60 +36,75 @@ namespace MusicManagerTESTS
                     from c in db.Users
                     where c.UserName == "Padge"
                     select c;
-
+                var selectedTabs =
+                    from t in db.Tabs
+                    where t.TabName == "Through The Looking Glass"
+                    select t;
                 db.Users.RemoveRange(selectedUsers);
+                db.Tabs.RemoveRange(selectedTabs);
                 db.SaveChanges();
             }
         }
 
 
-        //[Test]
-        //public void WhenANewUserIsAdded_NumberOfUsersIncreasedBy1()
-        //{
-        //    using (var db = new MusicManagerContext())
-        //    {
-        //        int initialusercount = db.Users.Count();
-        //        string returnMessage = _crudManager.AddUser("Padge", "Padge");
-        //        Assert.AreEqual(db.Users.Count(), initialusercount + 1);
-        //        Assert.AreEqual(returnMessage, "User Successfully Created");
-        //    }
-        //}
+        [Test]
+        public void WhenANewUserIsAdded_NumberOfUsersIncreasedBy1()
+        {
+            using (var db = new MusicManagerContext())
+            {
+                int initialusercount = db.Users.Count();
+                var returnMessage = _crudManager.AddUser("Padge", "Padge123");
+                Assert.AreEqual(db.Users.Count(), initialusercount + 1);
+                Assert.AreEqual(returnMessage.returnMessage, "User Successfully Created.");
+                Assert.AreEqual(returnMessage.passOrFail, "pass");
+            }
+        }
 
         //[Test]
         //public void CannotAddAUserWhenUsernameAlreadyInUse()
         //{
         //    using (var db = new MusicManagerContext())
         //    {
+               
         //        int initialusercount = db.Users.Count();
-        //        string returnMessage = _crudManager.AddUser("Noodle", "noodle");
-        //        Assert.AreEqual(initialusercount, db.Users.Count());
-        //        Assert.AreEqual(returnMessage, "This username is already in use");
-        //    }
-        //}
-
-
-        //[Test]
-        //public void NewTabAddedToUser()
-        //{
-        //    using (var db = new MusicManagerContext())
-        //    {
         //        var newUser = new User
         //        {
         //            UserName = "Padge",
         //            Password = "Padge"
         //        };
+        //        db.Users.Add(newUser);
 
-        //    _crudManager.AddTab("Through The Looking Glass", "Dream Theater" ,"Guitar", "www.sfgsgs.agsg", newUser);
-        //        var selectedUsers =
-        //            db.Tabs.Where(c => c.TabCreator == newUser.UserId);
-        //        foreach (var item in selectedUsers)
-        //        {
-        //            Assert.AreEqual("Through The Looking Glass", item.TabName);
-        //            Assert.AreEqual("Dream Theater", item.BandName);
-        //            Assert.AreEqual("Guitar", item.Instrument);
-        //            Assert.AreEqual("www.sfgsgs.agsg", item.TabUrl);
-        //        }
+        //        //var returnMessage = _crudManager.AddUser("Padge", "Padge123");
+        //        Assert.AreEqual(initialusercount, db.Users.Count());
+                
+        //        //Assert.AreEqual(returnMessage.returnMessage, "This username is already in use.");
+        //        //Assert.AreEqual(returnMessage.passOrFail, "fail");
         //    }
         //}
+
+
+        [Test]
+        public void NewTabAddedToUser()
+        {
+            using (var db = new MusicManagerContext())
+            {
+                var newUser = new User
+                {
+                    UserName = "Padge",
+                    Password = "Padge123"
+                };
+                db.Users.Add(newUser);
+                _crudManager.AddTab("Through The Looking Glass", "Dream Theater", "Guitar", "www.sfgsgs.agsg", newUser);
+                var selectedUsers =
+                    db.Tabs.Where(c => c.TabCreator == newUser.UserId);
+                foreach (var item in selectedUsers)
+                {
+                    Assert.AreEqual("Through The Looking Glass", item.TabName);
+                    Assert.AreEqual("Dream Theater", item.BandName);
+                    Assert.AreEqual("Guitar", item.Instrument);
+                    Assert.AreEqual("www.sfgsgs.agsg", item.TabUrl);
+                }
+            }
+        }
     }
 }
